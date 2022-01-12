06d59aab260dbaf65b35d1f52a8d3af4b5906f3c
KazancevaYuliya <87359439+KazancevaYuliya@users.noreply.github.com>
2022-01-12 16:55:29 +0300

    Add files via upload

---
+++ ДЗ/script.js
@@ -0,0 +1,53 @@
+// ---1.---
+
+//function clear(elem) {
+//}
+//const sectionElement = document.createElement('section')
+//sectionElement.innerHTML = '<ol><li>Привет</li><li>Мир</li></ol>'
+//document.body.append(sectionElement)
+//console.log(sectionElement)
+
+//document.body.innerHTML = ''
+
+
+
+
+// ---2.---
+
+//const ul = document.createElement('ul')
+//document.body.append(ul)
+
+//while (true) {
+//const paragraph = prompt('Введите текст для элемента списка')
+
+//if (!paragraph) {
+//break
+//}
+
+//const li = document.createElement('li')
+//li.textContent = paragraph
+//ul.append(li)
+//}
+
+// ---3.---
+
+//let linksArr = [
+    //'https://geekytheory.com/assets/images/icons/technologies/javascript.png',
+    //'https://cdn-media-1.freecodecamp.org/images/1*kt9otqHk14BZIMNruiG0BA.png',
+    //'https://cdn.searchenginejournal.com/wp-content/uploads/2019/04/the-seo-guide-to-angular-1520x800.png',
+    //'https://habrastorage.org/webt/jp/qj/fk/jpqjfkjewyfpm1cbr5yxaubxt-w.png',
+    //'https://miro.medium.com/max/700/1*m0UdF7LWONe3lXtfbfiS5g.png'
+//];
+
+//const divElement = document.createElement('div')
+//divElement.style.width = '50%'
+//divElement.style.height = '50%'
+//console.log(divElement)
+
+//linksArr.forEach(function (item) {
+    //const linksArrElement = document.createElement('img')
+    //linksArrElement.setAttribute('src', item)
+    //divElement.append(linksArrElement)
+
+    //document.body.append(divElement)
+//})
